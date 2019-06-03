5f9697cad774a32e26b7cb43d84c3eff89a4f634
acoltelli <acoltelli@protonmail.com>
2019-06-03 16:38:57 -0700

    add conditional rendering

--- src/App.js
+++ src/App.js
@@ -3,28 +3,19 @@ import { Component } from "react";
 import './App.css';
 
 
-class CountdownTimer extends React.Component {
+class Timer extends React.Component {
         constructor(props) {
           super(props);
           this.state = {
-            timeRemaining: 0,
+            timeRemaining: 0, //in seconds
             seconds: 0,
             minutes: 0,
-            hours: 0
+            hours: 0,
+            timerActive: false,
+            buttonPress: false
           };
-          this.handleChange = this.handleChange.bind(this);
-          this.handleSubmit = this.handleSubmit.bind(this);
-        }
-
-        increaseTimer(){
-          this.setState({timeRemaining: this.state.timeRemaining + 60})
-        }
-
-        decreaseTimer(){
-          if (this.state.timeRemaining > 0)  {
-            this.setState({timeRemaining: this.state.timeRemaining - 60})
-          }
-
+          this.helperChange = this.helperChange.bind(this);
+          this.helperSubmit = this.helperSubmit.bind(this);
         }
 
         getHours() {
@@ -39,66 +30,102 @@ class CountdownTimer extends React.Component {
           return ("0" + (this.state.timeRemaining % 60)).slice(-2);
         }
 
-        startTimer() {
-          var _this = this;
-          this.countdown = setInterval(dec, 1000); //decrements timeRemaining by one every 1000ms.
-          function dec(){
-            if (_this.state.timeRemaining > 0){
-            _this.setState({ timeRemaining: _this.state.timeRemaining - 1 });
-          }
-          //clear timer when count is zero
-          if (_this.state.timeRemaining == 0){
-          clearInterval(this.countdown);
-        }}
-        }
-
         resetTime() {
           this.reset = this.setState({
-            timeRemaining: (this.state.timeRemaining = 0)
+            timeRemaining: (this.state.timeRemaining = 0),
+            seconds: 0,
+            minutes: 0,
+            hours: 0,
+            timerActive: false
+
           });
           clearInterval(this.countdown);
         }
 
-        pauseTime() {
+        pauseButton() {
           clearInterval(this.countdown);
+          this.setState({
+            buttonPress: true
+          });
         }
 
-        handleChange(event) {
+        startButton(){
+          this.setState({
+            buttonPress: false
+          });
+          this.startTimer();
+        }
+
+        startTimer() {
+          var _this = this;
+          this.countdown = setInterval(timer, 1000);
+          function timer(){
+            if (_this.state.timeRemaining > 0){
+              _this.setState({ timeRemaining: _this.state.timeRemaining - 1 });
+          }
+            if (_this.state.timeRemaining == 0){
+              _this.resetTime();
+        }}
+        }
+
+        helperChange(event) {
           var num = parseInt(event.target.value, 10);
           var name = event.target.name;
           this.setState({ [name]: num });
         }
 
-        handleSubmit(event) {
+        helperSubmit(event) {
          event.preventDefault();
          var totalSeconds = (this.state.hours * 3600) + (this.state.minutes * 60) + (this.state.seconds);
-         this.setState({timeRemaining: totalSeconds});
+         if (totalSeconds == 0){
+           alert("Please type a value into the countdown timer");
+           return;
+         }
+         this.setState({
+           timeRemaining: totalSeconds,
+             seconds: 0,
+             minutes: 0,
+             hours: 0,
+             timerActive: true  });
+             this.helper();
+       }
+
+        helper(){
+         this.startTimer();
        }
 
         render() {
           return (
-
             <div>
             <h2 className = "TimerTitle">Countdown Timer </h2>
+              <div className='conditional'>
 
-              <div className="clock">
-                {this.state.hours}:{this.state.minutes}:{this.state.seconds}
-                <h1/>
-                Actual timer : {this.getHours()}:{this.getMinutes()}:{this.getSeconds()}
+              {(this.state.timerActive == false) ?
+                <div className= "clock">
 
-                <form onSubmit={this.handleSubmit}>
-                <input type="number" name="hours" onChange={this.handleChange} placeholder="00"/>:
-                <input type="number"  min="00" max="59" name="minutes" onChange={this.handleChange} placeholder="00"/>:
-                <input type="number" min="00" max="59" name="seconds" onChange={this.handleChange} placeholder="00"/>
-                <input type="submit" value="Submit" />
-                </form>
+                  <form onSubmit={this.helperSubmit}>
+                  <input type="text" name= "hours" pattern="\d*" onChange={this.helperChange} placeholder="00" className='hours'/>:
+                  <input type="text" maxLength= "2" pattern="\d*" name="minutes" onChange={this.helperChange} placeholder="00"/>:
+                  <input type="text" maxLength= "2" pattern="\d*" name="seconds" onChange={this.helperChange} placeholder="00"/>
+                  <div>
+                  <input type="submit" value="Begin"/>
+                  </div>
+                  </form>
+
+                </div>
+                :
+                <div className = "CountdownTimer ">
+
+              <div className= "clock">{this.getHours()}:{this.getMinutes()}:{this.getSeconds()}
+              <div>
+              {this.state.buttonPress ? <button onClick={() => this.startButton()}>Start</button> : <button onClick={() => this.pauseButton()}>Pause</button>}
+              <button onClick={() => this.resetTime()}>Reset</button>
+              </div>
+                </div>
 
               </div>
+            }
 
-              <div className = "buttonPanel">
-              <button onClick={() => this.startTimer()}>Start</button>
-              <button onClick={() => this.pauseTime()}>Pause</button>
-              <button onClick={() => this.resetTime()}>Reset</button>
               </div>
             </div>
           );
@@ -106,15 +133,12 @@ class CountdownTimer extends React.Component {
       }
 
 
-
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <CountdownTimer />
+        <Timer />
       </div>
     ); }};
-
 
 export default App;
